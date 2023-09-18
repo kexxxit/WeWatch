edaf4493c65fb7239267165cdbd3c5a1f999b7a0
kexxxit <kvashevich80@gmail.com>
2023-09-18 23:26:08 +0700

    Initial commit

---
+++ app/src/main/java/com/sample/wewatch/main/MainContract.kt
@@ -0,0 +1,18 @@
+package com.sample.wewatch.main
+
+import com.sample.wewatch.model.Movie
+
+class MainContract {
+    interface PresenterInterface {
+        fun getMyMoviesList()
+        fun stop()
+    }
+
+    interface ViewInterface {
+        fun displayMovies (movieList: List <Movie>)
+        fun displayNoMovies ()
+        fun displayMessage (message: String )
+        fun displayError (message: String )
+    }
+
+}
